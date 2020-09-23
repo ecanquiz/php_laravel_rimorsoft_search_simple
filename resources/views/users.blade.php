c05a8db8297df7ae73489df767fbfdb771dc23bf
Ernesto Canquiz <fdsoil123@gmail.com>
2020-09-23 06:58:05 -0400

    Lesson 4 - User Pagination

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -14,8 +14,7 @@
       <div class="row">
 	<div class="col-md-12">
           <div class="page-header">
-	   <h1>
-	     Búsqueda de usuarios             
+	   <h1>Búsqueda de usuarios             
              <form action="{{ route('users') }}" method="GET" class="form-inline pull-right">
 	       <div class="form-group">
 		 <input type="text"
@@ -35,13 +34,28 @@
                         class="form-control"
                         placeholder="Bio"/>
                </div>
-               <divi class="form-group">
+               <div class="form-group">
 		 <button type="submit" class="btn btn-default">
                    <span class="glyphicon glyphicon-search"></span> 
                  </button>
                </div>
              </form>
-           </h1> 
+            </h1>
+          </div>
+	  <div class="col-md-8">
+	    <table class="table table-hover table-striped">
+	      <tbody>
+                @foreach($users as $user)
+		<tr>
+                  <td>{{$user->id}}</td>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  <td>{{$user->bio}}</td>
+                </tr>
+                @endforeach
+              </tbody>
+            </table>
+            {{ $users->render() }}
           </div>
         </div>
       </div>    
